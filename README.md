8010af825247907cb3d082cce527429d956c35fd
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-18 08:33:51 -0600

    formatted milestone 1 for html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pwp
+# This is the personal website project of Matthew R Fisher
+# This website will serve as a web based business card
--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,3 +5,44 @@
  * Date: 10/17/2016
  * Time: 4:37 PM
  */
+?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthew
+ * Date: 10/12/2016
+ * Time: 11:00 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Milestone-1</title>
+	</head>
+	<body>
+		<!-- the header -->
+		<header>
+			<div class="header-main-text">
+				<h1>Milestone-1</h1>
+			</div>
+
+		</header>
+
+		<main>
+
+
+				<section>
+
+				</section>
+
+				<section>
+
+				</section>
+
+
+		</main>
+
+	</body>
+</html>
+

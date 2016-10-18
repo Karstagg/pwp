14ff8bd902319472e30676c73430fa759b2f805f
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-18 11:24:15 -0600

    added purpose and persona to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,27 +18,37 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Milestone-1</title>
+		<title>PWP Milestone-1</title>
 	</head>
 	<body>
 		<!-- the header -->
 		<header>
-			<div class="header-main-text">
-				<h1>Milestone-1</h1>
-			</div>
+
+			<h1>PWP Milestone-1</h1>
 
 		</header>
 
 		<main>
 
+			<section>
+				<br>
+				<h2>Purpose, Audience, and Goal</h2>
+				<p><strong>Purpose:</strong> This site is an electronic business card and a portfolio that will showcase my front end development skills and eventually link to my other projects.</p>
+				<p><strong>Audience:</strong> This site is designed for a professional audience, particularly members of the web development community both in the U.S. and abroad. <br> The target audience includes recruiters, hiring managers, peers, and startup founders who need a web developer</p>
+				<p><strong>Goal:</strong> The goal of this site is for people who may have possible work opportunities for me to be able to learn about my background, see some of my work, <br> and reach out to me, either through the site or through linked social media accounts.</p>
+			</section>
 
-				<section>
-
-				</section>
-
-				<section>
-
-				</section>
+			<section>
+				<br>
+				<h2>Persona</h2>
+				<p><strong>Name:</strong> Johan Leckershmeck</p>
+				<p><strong>Age:</strong> 38</p>
+				<p><strong>Profession:</strong> Is a hiring manager at a start up web development company. The company has just received several new major contracts and needs to hire a new full time web developer.</p>
+				<p><strong>Technology:</strong> Johan mostly uses Windows, but is also familiar with Linux. He despises Apple and refuses to work with their products unless it is completely unavoidable. <br> Additionally, he frequently uses his Android phone to browse the internet and use apps.</p>
+				<p><strong>Attitudes and Behaviors:</strong> Johan was incredibly overworked before the company decided to look for a new developer. Now he has to put in additional overtime to review applications. <br> Because of this he generally only spends two to three minutes looking at portfolio sites and just a few seconds glossing over resumes.<br> He also likes to check up on prospective employees social media accounts but spends only a minute or two looking at these. <br> If a portfolio site catches his eye he may spend a few minutes reviewing that person's github.</p>
+				<p><strong>Frustrations and needs:</strong> Johan's team is over worked and he really needs another web developer. The company has some new contracts but they still cannot afford to hire a high level or even mid level developer. <br> That being said Johan is not impressed by most entry level candidates and wants to hire someone with more experience but cannot afford to pay them what they are worth from the start.</p>
+				<p><strong>Goal:</strong> Johan's goal is to be able to find someone who really stands out as a promising addition to his team just be taking a quick look at their portfolio site. <br> He want's someone who seems like they know what they are doing and he wants to see some personality in the site, he wants the new hire to get along with the rest of his team.</p>
+			</section>
 
 
 		</main>

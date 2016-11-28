122462a9aa49664129ee975ab512b0194e891927
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-28 08:43:24 -0700

    changed sizing on navbar text

--- public_html/content-jp.php
+++ public_html/content-jp.php
@@ -6,10 +6,10 @@
 		<script src="js/lib.js"></script>
 	</head>
 	<body>
-		<div id="morning-jp"><h1>おはようございます</h1></div>
+		<div id="morning-jp"><h1>おはようございます！</h1></div>
 		<div id="afternoon-jp"><h1>こんにちは！</h1></div>
 		<div id="evening-jp"><h1>こんばんは！</h1></div>
-		<div id="intro-jp"><p>フィシャー・マットと申します。ウェブデブぇローパーです。このページは自分のページとポートフォリオです。他の作を見て、現行のプロジェクトについて読んで、連絡して下さい。</p></div>
+		<div id="intro-jp"><p>フィシャー・マットと申します。ウェブデブぇローパーです。自分のページとポートフォリオです。他の作を見て、現行のプロジェクトについて読んで、連絡して下さい。</p></div>
 		<div></div>
 	</body>
-</html>
+</html>！

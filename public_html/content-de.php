9b8f6352dcfa5234010a7c8671d967847f41f8ed
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-27 20:22:21 -0700

    included dependencies for setting up the swift mailer form

--- public_html/content-de.php
+++ public_html/content-de.php
@@ -9,7 +9,7 @@
 		<div id="morning-de"><h1>Guten Morgen!</h1></div>
 		<div id="afternoon-de"><h1>Guten Tag!</h1></div>
 		<div id="evening-de"><h1>Guten Abend!</h1></div>
-		<div id="intro-de"><p>hier können Sie auf meine andere Arbeit blicken, über mein aktuelle Arbeit lesen, oder Mich kontaktieren.</p></div>
+		<div id="intro-de"><p>Ich heiße Matthew Fisher und das ist mein persönlicher website und portfolio. hier können Sie auf meine andere Arbeit blicken, über mein aktuelle Arbeit lesen, oder Mich kontaktieren.</p></div>
 		<div></div>
 	</body>
 </html>
--- public_html/index.php
+++ public_html/index.php
@@ -25,9 +25,13 @@
 		<![endif]-->
 		<!-- latest min jQuery and Bootstrap-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="http://malsup.github.com/jquery.form.js"></script>
+		<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+		<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/additional-methods.min.js"></script>
+
+
+
 		<script src="js/lib.js"></script>
 		<title>Matthew R. Fisher</title>
 	</head>

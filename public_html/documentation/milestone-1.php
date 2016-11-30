164dfef2e3ab04b00cf616cc3c4dcfcc2ba58d23
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-30 08:49:28 -0700

    added an html template

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,19 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Matthew
- * Date: 10/17/2016
- * Time: 4:37 PM
- */
-?>
-<?php
-/**
- * Created by PhpStorm.
- * User: Matthew
- * Date: 10/12/2016
- * Time: 11:00 AM
- */
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
